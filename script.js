31582998206d18ab6d16b3ea0cc22239958343e6
Wesley150889 <150889@student.horizoncollege.nl>
2024-06-28 00:43:02 +0200

    RaveWaveV2
    lichte animaties

--- script.js
+++ script.js
@@ -32,6 +32,37 @@ setInterval(function countdown(){
 
 })}
 
+function synthesizer1(){
+    document.querySelector(".block1").style.animation = "block1 1s infinite"
+    
+}
+function synthesizer2(){
+    document.querySelector(".block2").style.animation = "block2 1s infinite"
+    
+}
+function synthesizer3(){
+    document.querySelector(".block3").style.animation = "block3 1s infinite"
+
+}
+function synthesizer4(){
+    document.querySelector(".block4").style.animation = "block4 1s infinite"
+}
+function synthesizer6(){
+    document.querySelector(".block6").style.animation = "block6 1s infinite"
+    
+}
+function synthesizer7(){
+    document.querySelector(".block7").style.animation = "block7 1s infinite"
+    
+}
+function synthesizer8(){
+    document.querySelector(".block8").style.animation = "block8 1s infinite"
+    
+}
+function synthesizer9(){
+    document.querySelector(".block9").style.animation = "block9 1s infinite"
+}
+
 function restart(){
     timer = 300000
     timed()
@@ -46,6 +77,14 @@ if(timer > 0){
     setTimeout(hits.pause(),1000)
     house.play()
     timed()
+    synthesizer1()
+    setTimeout(synthesizer2,500)
+    setTimeout(synthesizer6,500)
+    setTimeout(synthesizer3,1000)
+    setTimeout(synthesizer7,1000)
+    setTimeout(synthesizer4,1500)
+    setTimeout(synthesizer8,1500)
+    setTimeout(synthesizer9,2000)
 } else{
     restart()
     console.log("test")
@@ -57,11 +96,20 @@ if(timer > 0){
 function musictechno(){
 if(timer > 0){
     time = true
+    synthesizer()
     setTimeout(house.pause(),1000)
     setTimeout(drumbass.pause(),1000)
     setTimeout(hits.pause(),1000)
     techno.play()
     timed()
+    synthesizer1()
+    setTimeout(synthesizer2,500)
+    setTimeout(synthesizer6,500)
+    setTimeout(synthesizer3,1000)
+    setTimeout(synthesizer7,1000)
+    setTimeout(synthesizer4,1500)
+    setTimeout(synthesizer8,1500)
+    setTimeout(synthesizer9,2000)
 } else{
     restart()
     }
@@ -70,11 +118,20 @@ if(timer > 0){
 function musicdrumbass(){
 if(timer > 0){
     time = true
+    synthesizer()
     setTimeout(house.pause(),1000)
     setTimeout(techno.pause(),1000)
     setTimeout(hits.pause(),1000)
     drumbass.play()
     timed()
+    synthesizer1()
+    setTimeout(synthesizer2,500)
+    setTimeout(synthesizer6,500)
+    setTimeout(synthesizer3,1000)
+    setTimeout(synthesizer7,1000)
+    setTimeout(synthesizer4,1500)
+    setTimeout(synthesizer8,1500)
+    setTimeout(synthesizer9,2000)
 } else{
     restart()
 }
@@ -83,11 +140,20 @@ if(timer > 0){
 function musichits(){
 if(timer > 0){
     time = true
+    synthesizer()
     setTimeout(house.pause(),1000)
     setTimeout(techno.pause(),1000)
     setTimeout(drumbass.pause(),1000)
     hits.play()
     timed()
+    synthesizer1()
+    setTimeout(synthesizer2,500)
+    setTimeout(synthesizer6,500)
+    setTimeout(synthesizer3,1000)
+    setTimeout(synthesizer7,1000)
+    setTimeout(synthesizer4,1500)
+    setTimeout(synthesizer8,1500)
+    setTimeout(synthesizer9,2000)
 } else{
     restart()
 }
